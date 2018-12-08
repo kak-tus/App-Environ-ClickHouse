956a049094a75f56881176e066aa216bc5168a86
Andrey Kuzmin <prepodam@mail.ru>
2018-12-08 13:59:11 +0300

    Support for async requests with AnyEvent::ClickHouse.

--- example/example.pl
+++ example/example.pl
@@ -8,6 +8,7 @@ use v5.10;
 use lib 'lib';
 $ENV{APPCONF_DIRS} = 'example';
 
+use AE;
 use App::Environ;
 use App::Environ::ClickHouse;
 use Data::Dumper;
@@ -16,7 +17,29 @@ App::Environ->send_event('initialize');
 
 my $CH = App::Environ::ClickHouse->instance;
 
-my $data = $CH->selectall_hash('SELECT * FROM default.test');
+my $data = $CH->selectall_hash('SELECT 1');
 say Dumper $data;
+
+my $async = App::Environ::ClickHouse->async;
+
+my $cv = AE::cv;
+
+$async->selectall_hash(
+  'SELECT 1',
+  sub {
+    my ( $data, $err ) = @_;
+
+    if ($err) {
+      say $err;
+      $cv->send;
+      return;
+    }
+
+    say Dumper $data;
+    $cv->send;
+  }
+);
+
+$cv->recv;
 
 App::Environ->send_event('finalize:r');
--- lib/App/Environ/ClickHouse.pm
+++ lib/App/Environ/ClickHouse.pm
@@ -1,17 +1,21 @@
 package App::Environ::ClickHouse;
 
-our $VERSION = '0.2';
+our $VERSION = '0.3.1';
 
 use strict;
 use warnings;
 use v5.10;
 use utf8;
 
+use AnyEvent::ClickHouse qw();
 use App::Environ;
 use App::Environ::Config;
+use Carp qw(croak);
 use HTTP::ClickHouse;
+use Params::Validate qw(validate_pos);
 
 my $INSTANCE;
+my $ASYNC;
 
 App::Environ->register( __PACKAGE__, postfork => sub { undef $INSTANCE } );
 
@@ -34,13 +38,45 @@ sub instance {
   return $INSTANCE;
 }
 
+sub async {
+  my $class = shift;
+
+  unless ($ASYNC) {
+    my $config = App::Environ::Config->instance;
+
+    my %opt = (
+      host     => $config->{clickhouse}{host},
+      port     => $config->{clickhouse}{port},
+      database => '',
+    );
+
+    $ASYNC = bless { opt => \%opt };
+  }
+
+  return $ASYNC;
+}
+
+sub selectall_hash {
+  my __PACKAGE__ $self = shift;
+
+  my $cb = pop;
+  croak 'No cb' unless $cb;
+
+  my ($sql) = validate_pos( @_, 1 );
+
+  AnyEvent::ClickHouse::clickhouse_select_hash( $self->{opt}, $sql, $cb,
+    sub { $cb->( undef, @_ ) } );
+
+  return;
+}
+
 1;
 
 __END__
 
 =head1 NAME
 
-App::Environ::ClickHouse - get instance of HTTP::ClickHouse in App::Environ environment
+App::Environ::ClickHouse - get instance of HTTP::ClickHouse or AnyEvent::ClickHouse in App::Environ environment
 
 =head1 SYNOPSIS
 
@@ -52,8 +88,30 @@ App::Environ::ClickHouse - get instance of HTTP::ClickHouse in App::Environ envi
 
   my $CH = App::Environ::ClickHouse->instance;
 
-  my $data = $CH->selectall_hash('SELECT * FROM default.test');
+  my $data = $CH->selectall_hash('SELECT 1');
   say Dumper $data;
+
+  my $async = App::Environ::ClickHouse->async;
+
+  my $cv = AE::cv;
+
+  $async->selectall_hash(
+    'SELECT 1',
+    sub {
+      my ( $data, $err ) = @_;
+
+      if ($err) {
+        say $err;
+        $cv->send;
+        return;
+      }
+
+      say Dumper $data;
+      $cv->send;
+    }
+  );
+
+  $cv->recv;
 
   App::Environ->send_event('finalize:r');
 
